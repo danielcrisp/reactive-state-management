e57e54648c24aad2d99a95f45e296e2930ee1ee7
danielcrisp <daniel@danielcrisp.com>
2018-04-25 17:31:18 +0100

    Step 17

--- demo/src/app/search/components/index/index.component.ts
+++ demo/src/app/search/components/index/index.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { select } from '@angular-redux/store';
+
+import { SearchActionsService } from '../../store/search.actions';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
-export class IndexComponent implements OnInit {
+export class IndexComponent {
 
-  constructor() { }
+  // Selects properties from the store and creates Observables
+  @select(['search', 'q']) q$: Observable<string>;
+  @select(['search', 'pages']) pages$: Observable<any[]>;
+  @select(['search', '_loading']) loading$: Observable<boolean>;
+  @select(['search', '_error']) error$: Observable<boolean>;
 
-  ngOnInit() {
+  // The value of the input
+  query: string;
+
+  constructor (private searchActions: SearchActionsService) { }
+
+  onSubmit () {
+    // Dispatches a FETCH_PAGES action with the value of the input
+    this.searchActions.fetchPages({
+      q: this.query
+    });
   }
-
 }
