a3566f2fa7e1953b8d0563fdd57e3679a1765bbe
danielcrisp <daniel@danielcrisp.com>
2018-04-25 17:11:16 +0100

    Step 7

--- demo/src/app/app-routing.module.ts
+++ demo/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { RootComponent } from './@shared/components/root/root.component';
+
+const routes: Routes = [{
+  path: '',
+  component: RootComponent
+}, {
+  path: 'search',
+  loadChildren: './search/search.module#SearchModule'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
