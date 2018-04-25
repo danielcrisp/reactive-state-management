8bc6fb105a089780ff03e2aafb2748117c8184fc
danielcrisp <daniel@danielcrisp.com>
2018-04-25 17:27:47 +0100

    Step 14

--- demo/src/app/search/search.module.ts
+++ demo/src/app/search/search.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { SearchRoutingModule } from './search-routing.module';
 import { SearchStoreModule } from './search-store.module';
@@ -8,6 +10,8 @@ import { IndexComponent } from './components/index/index.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    HttpClientModule,
     SearchRoutingModule,
     SearchStoreModule
   ],
