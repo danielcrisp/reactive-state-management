2f5018f16bc42346226cb33b3d6046694d829db4
danielcrisp <daniel@danielcrisp.com>
2018-04-25 17:33:17 +0100

    Step 18

--- demo/src/app/search/store/search.epics.ts
+++ demo/src/app/search/store/search.epics.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpErrorResponse } from '@angular/common/http';
+
 import { NgRedux } from '@angular-redux/store';
 import { ActionsObservable } from 'redux-observable';
-import { switchMap } from 'rxjs/operators';
+
+import { catchError, concat, switchMap, takeUntil } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
 import { empty } from 'rxjs/observable/empty';
 
 import { SearchActionsService } from './search.actions';
@@ -9,7 +13,12 @@ import { SearchActionsService } from './search.actions';
 @Injectable()
 export class SearchEpicsService {
 
+  constructor (private http: HttpClient) {}
+
   onFetchPages = (action$: ActionsObservable<any>, store: NgRedux<any>) => {
+
+    const cancel$ = action$.ofType(SearchActionsService.FETCH_PAGES);
+
     return action$.ofType(SearchActionsService.FETCH_PAGES).pipe(
       switchMap(({
         payload
